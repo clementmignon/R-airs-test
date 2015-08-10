e2c94282b3001ce8dedae761a24d030deb5f81f4
unknown <NG4AD97@BLLX300066436.tls.fr.eu.airbus.corp>
2015-08-10 12:27:32 +0200

    First version

---
+++ test.R
@@ -0,0 +1,6 @@
+library(rCharts)
+library(ggplot2)
+
+## Example 1 Facetted Scatterplot
+names(iris) = gsub("\\.", "", names(iris))
+rPlot(SepalLength ~ SepalWidth | Species, data = iris, color = 'Species', type = 'point')
